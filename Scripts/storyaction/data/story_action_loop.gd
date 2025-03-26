35e25f5b33fe682f3a294ebc46fd1333e7960b17
snipercup <50166150+snipercup@users.noreply.github.com>
2025-03-26 23:39:51 +0100

    Implement max loops

--- Scripts/storyaction/data/story_action_loop.gd
+++ Scripts/storyaction/data/story_action_loop.gd
@@ -3,16 +3,22 @@ extends StoryAction
 
 var ui_scene: PackedScene = preload("res://Scenes/action/story_action_loop_ui.tscn")
 var cooldown: float = 1.0
+# Add this line to declare the new property with a default value
+var max_loops: int = -1
 
 func _init(data: Dictionary, myarea: StoryArea = null) -> void:
-	super(data, myarea)  # Call parent class _init function
+	super(data, myarea)
 	cooldown = data.get("cooldown", 1.0)
+	max_loops = data.get("max_loops", -1)  # Retrieve from data or use default
 
 func perform_action() -> void:
 	print("Performed loop action: %s" % [story_text])
 
 func get_cooldown() -> float:
 	return cooldown
+
+func get_max_loops() -> float:
+	return max_loops
 
 func get_icon() -> String:
 	return "🔁"
