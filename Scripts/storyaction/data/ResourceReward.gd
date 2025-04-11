f2b0ef21710984fac540d194bd3a89c5d041d327
snipercup <50166150+snipercup@users.noreply.github.com>
2025-04-11 16:20:36 +0200

    Allow temporary capacity

--- Scripts/storyaction/data/ResourceReward.gd
+++ Scripts/storyaction/data/ResourceReward.gd
@@ -6,7 +6,9 @@ extends RefCounted
 var temporary: float = 0.0
 var permanent: float = 0.0
 var regeneration: float = 0.0
-var capacity: float = 0.0
+var temporary_capacity: float = 0.0
+var permanent_capacity: float = 0.0
+
 
 # Example rewards json:
 #	  "rewards": {
@@ -14,7 +16,8 @@ var capacity: float = 0.0
 #		"Focus": { "regeneration": 0.1 }, //Adds to regeneration amount
 #		"Perception": { "permanent": 1 }, //Adds to permanent amount
 #		"Perception": { "temporary": 1 }, //Adds to temporary amount
-#		"Story Points": { "capacity": 10 }, //adds to max capacity
+#		"Story Points": { "permanent_capacity": 10 }, //adds to permanent max capacity
+#		"Story Points": { "temporary_capacity": 10 }, //adds to temporary max capacity
 #		"h_hidden_rat_reward": 1.0 //Adds to temporary amount
 #	  }
 
@@ -26,8 +29,8 @@ func from_dict(data: Dictionary) -> void:
 	temporary = data.get("temporary", 0.0)
 	permanent = data.get("permanent", 0.0)
 	regeneration = data.get("regeneration", 0.0)
-	capacity = data.get("capacity", 0.0)
-
+	temporary_capacity = data.get("temporary_capacity", 0.0)
+	permanent_capacity = data.get("permanent_capacity", 0.0)
 
 # === MANIPULATION ===
 
@@ -35,12 +38,18 @@ func add_to(other: ResourceReward) -> void:
 	temporary += other.temporary
 	permanent += other.permanent
 	regeneration += other.regeneration
-	capacity += other.capacity
+	temporary_capacity += other.temporary_capacity
+	permanent_capacity += other.permanent_capacity
 
 
 func is_empty() -> bool:
-	return temporary == 0 and permanent == 0 and regeneration == 0 and capacity == 0
-
+	return (
+	temporary == 0 and
+	permanent == 0 and
+	regeneration == 0 and
+	temporary_capacity == 0 and
+	permanent_capacity == 0
+)
 
 # === APPLICATION ===
 
@@ -49,7 +58,8 @@ func apply_to(resource: ResourceData) -> void:
 	resource.add_temporary(temporary)
 	resource.add_permanent(permanent)
 	resource.regeneration += regeneration
-	resource.add_capacity(capacity)
+	resource.add_temporary_capacity(temporary_capacity)
+	resource.add_permanent_capacity(permanent_capacity)
 
 
 # === SERIALIZATION ===
@@ -59,5 +69,6 @@ func to_dict() -> Dictionary:
 		"temporary": temporary,
 		"permanent": permanent,
 		"regeneration": regeneration,
-		"capacity": capacity
+		"temporary_capacity": temporary_capacity,
+		"permanent_capacity": permanent_capacity
 	}
