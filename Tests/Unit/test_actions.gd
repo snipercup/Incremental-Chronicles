fb5bde6b9f74c97c5156e3786127198704294bc0
snipercup <50166150+snipercup@users.noreply.github.com>
2025-04-17 21:36:52 +0200

    Second reincarnation

--- Tests/Unit/test_actions.gd
+++ Tests/Unit/test_actions.gd
@@ -385,8 +385,43 @@ func my_test_sequence(stop_area_name := "", delay_seconds := 0.0) -> bool:
 			await _press_all_actions_of_type(action_list, "free") # close the last book
 			await _press_actions_of_type(action_list,myactionquery,1) # Press patrol loop
 			await _wait_for_action_type_count(action_list, "free", 1, 15, 0.2) # One free action remains
-
+			# Keep pressing free actions until 0 remain
+			await _wait_for_action_type_count(action_list, "free", 0, 15, 0.2, _press_all_actions_of_type.bind(action_list, "free"))
 			
+			#-- HOLLOWFANG GORGE --
+			if await _open_area(area_list, "Hollowfang Gorge", stop_area_name): return true
+			# Keep pressing free actions until two remain. At this point, we learn that
+			# We lack strength or wisdom+insight, and have to reincarnate again.
+			await _wait_for_action_type_count(action_list, "free", 2, 15, 0.2, _press_all_actions_of_type.bind(action_list, "free"))
+			
+			#-- TEMPLE --
+			if await _open_area(area_list, "Forgotten Temple", stop_area_name): return true
+			# Keep pressing free actions until 1 remains
+			await _wait_for_action_type_count(action_list, "reincarnation", 1, 15, 0.2, _press_all_actions_of_type.bind(action_list, "free"))
+			query = ActionQuery.new("reincarnation") # Here we will reincarnate from the temple
+			await _press_actions_of_type(action_list, query, 1)
+			
+			 #-- REINCARNATION --
+			var is_visible := func(control: Control, expected: bool) -> bool:
+				return control.visible == expected
+			assert_true(await wait_until(is_visible.bind(area_list.areas_panel_container, false), 2, 0.5), "Expected area_list to be invisible")
+			assert_true(special_area_list.special_areas_panel_container.visible == true, "Expected special_area_list to be visible")
+			# We have entered the special reincarnation area, which holds actions
+			await _wait_for_action_type_count(action_list, "reincarnation", 1, 15, 0.2) # 1 reincarnation action
+			
+			# Get the echoes of the past
+			query = ActionQuery.new("free","","","Echoes of the Past")
+			await _press_actions_of_type(action_list, query, 1)
+			
+			# We get one echo from the reward list, then force 1 more. This will save reincarnating again
+			resources.apply_rewards({"Echoes of the Past": { "permanent": 1 }})
+			query.reward_key = "Strength" # We also get strength
+			await _press_actions_of_type(action_list, query, 1)
+			query.reward_key = "Story points" # Get the "Story points" max capacity buff
+			await _press_actions_of_type(action_list, query, 1)
+			query = ActionQuery.new("reincarnation") # Reincarnate and start over
+			await _press_actions_of_type(action_list, query, 1)
+
 	
 	if delay_seconds > 0.0: await wait_seconds(delay_seconds)
 	return false # No interruptions, so return false
